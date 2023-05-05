844f4742f4765eb39ec3c2319f0f37926db88041
ashish <ashishgaurav2403@gmail.com>
2023-05-05 17:57:13 +0530

    comment added

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
-    from .main import main as main_blueprint
+    
+    from .main import main as main_blueprint #blue print are added to make a map of of whole project / to structure the project
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
